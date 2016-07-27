0277e45445c063d00707c36079802b2a4e26aa5c
Ibrahim Sagiroglu <sag.ibrahim@gmail.com>
2016-07-27 13:58:13 +0300

    removable players

--- app/team_detail/team_detail.controller.js
+++ app/team_detail/team_detail.controller.js
@@ -4,13 +4,23 @@ class TeamDetailController {
     this.$http = $http;
   }
   $onInit() {
+    this.refresh();
+  }
+  removePlayer(player) {
+    let teamUrl = 'http://localhost:3000/players/' + player.id;
+    console.log(teamUrl);
+    this.$http.delete(teamUrl)
+      .then((result) => {
+        this.refresh();
+      });
+  }
+  refresh() {
     let teamUrl = 'http://localhost:3000/teams/' + this.$stateParams.id;
     console.log(teamUrl);
     this.$http.get(teamUrl)
       .then((result) => {
         this.teamDetail = result.data;
       });
-
   }
 }
 
